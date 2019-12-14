ae95c3f42f9d0bdf92b7d8edcadb0bf0f99168a0
Kirill Lepilov <supremko.fun@gmail.com>
2019-12-14 12:45:55 +0300

    change autotest method

--- src/test/java/hw1_1/CashbackHackServiceTest.java
+++ src/test/java/hw1_1/CashbackHackServiceTest.java
@@ -2,35 +2,20 @@ package hw1_1;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
-    @Test
-    void shouldReturnZero() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void calculateCashback(int amount, int expected, String message) {
+    CashbackHackService service = new CashbackHackService();
 
-        int result = service.remain(amount);
-        Assertions.assertTrue(result == 0);
-    }
+    int actual = service.remain(amount);
 
-    @Test
-    void shouldReturn400() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 600;
-
-        int result = service.remain(amount);
-        Assertions.assertTrue(result == 400);
-    }
-
-    @Test
-    void shouldReturn800() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 2200;
-
-        int result = service.remain(amount);
-        Assertions.assertTrue(result == 800);
+    assertEquals(expected, actual, message);
     }
 }
